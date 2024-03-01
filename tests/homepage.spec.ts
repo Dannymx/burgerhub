d16f47d0e90481adb9186982522e810708d9509b
Dan Saldana <hello@dan.mx>
2024-02-29 20:29:54 -0600

    feat: add basic playwright test

--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -1,7 +1,17 @@
 import test, { expect } from "@playwright/test";
 
-test("Home page has title", async ({ page }) => {
-  await page.goto("http://localhost:3000");
+test.describe("home", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("http://localhost:3000");
+  });
 
-  await expect(page).toHaveTitle("BurgerHub App");
+  test("homepage has title", async ({ page }) => {
+    await expect(page).toHaveTitle("BurgerHub App");
+  });
+
+  test("homepage has main heading", async ({ page }) => {
+    await expect(page.getByRole("navigation").getByRole("heading")).toHaveText(
+      "Burger",
+    );
+  });
 });
